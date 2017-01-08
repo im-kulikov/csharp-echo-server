9ef9251b8b6b06e8b8a9714088a96e18a8d11332
Evgeniy Kulikov <im@kulikov.im>
2017-01-08 06:56:14 +0300

    small fixes

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,25 @@
-FROM mono:onbuild
+FROM microsoft/dotnet:1.1-runtime-deps
 
-ENTRYPOINT /usr/bin/mono ./server.exe
+# Install .NET Core
+ENV DOTNET_DOWNLOAD_URL https://go.microsoft.com/fwlink/?LinkID=835021
+
+COPY ./Program.cs /usr/src/app/
+COPY ./project.json /usr/src/app/
+COPY ./project.lock.json /usr/src/app/
+
+WORKDIR /usr/src/app/
+
+RUN apt-get update -qq \
+    && apt-get install -y --no-install-recommends -qq \
+        curl \
+    && rm -rf /var/lib/apt/lists/* \
+    && curl -sSL $DOTNET_DOWNLOAD_URL --output dotnet.tar.gz \
+    && mkdir -p /usr/share/dotnet \
+    && mkdir -p /usr/src/app \
+    && tar -zxf dotnet.tar.gz -C /usr/share/dotnet \
+    && rm dotnet.tar.gz \
+    && ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet \
+    && /usr/bin/dotnet restore \
+    && /usr/bin/dotnet build --configuration Release
+
+CMD /usr/bin/dotnet run --configuration Release
--- Program.cs
+++ Program.cs
@@ -1,110 +1,33 @@
-﻿using System;
-using System.Net;
-using System.Threading;
-using System.Text;
-using System.Linq;
+﻿using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 
-namespace SimpleWebServer
+namespace LargeResponseApp
 {
-    public class WebServer
+    public class Startup
     {
-        private readonly HttpListener _listener = new HttpListener();
-        private readonly Func<HttpListenerRequest, string> _responderMethod;
+        byte[] data = Encoding.UTF8.GetBytes("Привет мир!");
 
-        public WebServer(string[] prefixes, Func<HttpListenerRequest, string> method)
+        public void Configure(IApplicationBuilder App)
         {
-            if (!HttpListener.IsSupported)
-                throw new NotSupportedException(
-                    "Needs Windows XP SP2, Server 2003 or later.");
-
-            // URI prefixes are required, for example 
-            // "http://localhost:8080/index/".
-            if (prefixes == null || prefixes.Length == 0)
-                throw new ArgumentException("prefixes");
-
-            // A responder method is required
-            if (method == null)
-                throw new ArgumentException("method");
-
-            foreach (string s in prefixes)
-                _listener.Prefixes.Add(s);
-            
-            _responderMethod = method;
-            _listener.Start();
-        }
-
-        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
-            : this(prefixes, method) { }
-
-        public void Run()
-        {
-            ThreadPool.QueueUserWorkItem((o) =>
+            App.Run(async (context) =>
             {
-                Console.WriteLine("Webserver running...");
-                try
-                {
-                    while (_listener.IsListening)
-                    {
-                        ThreadPool.QueueUserWorkItem((c) =>
-                        {
-                            var ctx = c as HttpListenerContext;
-                            try
-                            {
-                                string rstr = _responderMethod(ctx.Request);
-                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
-                                ctx.Response.ContentLength64 = buf.Length;
-                                ctx.Response.ContentType = "text/html; charset=utf8";
-                                ctx.Response.ContentEncoding = Encoding.UTF8;
-                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
-                                
-                            }
-                            catch { } // suppress any exceptions
-                            finally
-                            {
-                                // always close the stream
-                                ctx.Response.OutputStream.Close();
-                            }
-                        }, _listener.GetContext());
-                    }
-                }
-                catch { } // suppress any exceptions
+                context.Response.ContentType = "text/html; charset=utf-8";
+                context.Response.ContentLength = data.Length;
+                
+                await context.Response.Body.WriteAsync(data, 0, data.Length);
             });
         }
 
-        public void Stop()
+        public static void Main(string[] args)
         {
-            _listener.Stop();
-            _listener.Close();
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            WebServer ws = new WebServer(SendResponse, "http://+:8080/");
-            ws.Run();
-            Console.WriteLine("A simple webserver. Press Enter quit.");
+            var host = new WebHostBuilder()
+                .UseUrls("http://*:8080")
+                .UseKestrel(options => {})
+                .UseStartup<Startup>()
+                .Build();
 
-            Thread.Sleep(Timeout.Infinite);
-
-            //Under mono if you deamonize a process a Console.ReadLine with cause an EOF 
-            //so we need to block another way
-            if (args.Any(s => s.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
-            {
-                Thread.Sleep(Timeout.Infinite);
-            }
-            else
-            {
-                Console.ReadKey();
-            }
-            ws.Stop();
-        }
-
-        public static string SendResponse(HttpListenerRequest request)
-        {
-            return string.Format("Привет мир!");
-            
+            host.Run();
         }
     }
 }
-
